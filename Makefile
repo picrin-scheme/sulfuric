a3b01f087066cce2b8e117a0c75aea2ca3b4e38b
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-01-20 08:53:15 +0000

    add some tests against utils

---
+++ Makefile
@@ -0,0 +1,9 @@
+PICRIN=/home/kim/compile/picrin/build/bin/picrin
+
+all: test
+
+
+test: test-util
+
+test-util: piclib/util.scm t/util.scm
+	$(PICRIN) -l piclib/util.scm < t/util.scm
--- t/util.scm
+++ t/util.scm
@@ -1,3 +1,50 @@
 (import (scheme base)
-        (picrin test))
+        (picrin test)
+        (sulfuric util))
+
+(test-begin)
+
+(define-record-type-with-accessors <kons>
+  (kons kar kdr)
+  kons?
+  kar
+  kdr)
+(test #t (procedure? kar))
+(test #t (procedure? kdr))
+(test #t (procedure? set-kar!))
+(test #t (procedure? set-kdr!))
+(define k (kons 1 2))
+(test 1 (kar k))
+(test 2 (kdr k))
+(set-kar! k 2)
+(set-kdr! k 3)
+(test 2 (kar k))
+(test 3 (kdr k))
+
+(define lst ())
+
+(push! 1 lst)
+(test (list 1) lst)
+(push! 2 lst)
+(test 2 (pop! lst))
+(test (list 1) lst)
+(test 1 (pop! lst))
+(test () lst)
+
+(test () (extract! even? lst))
+(test () lst)
+(set! lst (list 1))
+(test () (extract! even? lst))
+(test (list 1) lst)
+
+(set! lst (list 2))
+(test (list 2) (extract! even? lst))
+(test () lst)
+
+(set! lst (list 1 2 3 4 5 6 7 8 9 10))
+
+(test (list 2 4 6 8 10) (extract! even? lst))
+(test (list 1 3 5 7 9) lst)
+
+(test-end)
 
